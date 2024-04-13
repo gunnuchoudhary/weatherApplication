804d295267ebd29d0cb259764d9a51f62d78abc4
rahul <rahul@timesgroup.com>
2024-04-13 15:58:30 +0530

    formatted

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import "./App.css";
-import React, { useState } from "react";
+import './App.css';
+import React, { useState } from 'react';
 function App() {
-  const [input, setInput] = useState();
-  return (
-    <div className="weather-App">
-      <h1 className="heading">Weather App</h1>
-      <input
-        className="inputCity"
-        placeholder="Enter City Name"
-        value={input}
-        onChange={(e) => {
-          console.log("01", e, "02", e.target);
-          setInput(e.target.value);
-        }}
-        // onKeyPress={search}
-      />
-    </div>
-  );
+    const [input, setInput] = useState();
+    return (
+        <div className='weather-App'>
+            <h1 className='heading'>Weather App</h1>
+            <input
+                className='inputCity'
+                placeholder='Enter City Name'
+                value={input}
+                onChange={(e) => {
+                    console.log('01', e, '02', e.target);
+                    setInput(e.target.value);
+                }}
+                // onKeyPress={search}
+            />
+        </div>
+    );
 }
 
 export default App;
